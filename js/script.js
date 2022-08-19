f533a01407c2140dcc0ed1e5a564701be84c35b9
Daniel Kim <danielkim2711@gmail.com>
2022-08-19 17:40:14 +1200

    Add overlay

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const btn = document.getElementById('menu-btn');
+const overlay = document.getElementById('overlay');
 
 btn.addEventListener('click', navToggle);
 
 function navToggle() {
   btn.classList.toggle('open');
+  overlay.classList.toggle('overlay-show');
 }
