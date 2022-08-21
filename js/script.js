f7c83783ab4e851cc0a3ac258ee1526762cf5bb2
Daniel Kim <danielkim2711@gmail.com>
2022-08-21 21:56:58 +1200

    Style mobile menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const btn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
+const menu = document.getElementById('mobile-menu');
 
 btn.addEventListener('click', navToggle);
 
@@ -7,4 +8,5 @@ function navToggle() {
   btn.classList.toggle('open');
   overlay.classList.toggle('overlay-show');
   document.body.classList.toggle('stop-scrolling');
+  menu.classList.toggle('show-menu');
 }
